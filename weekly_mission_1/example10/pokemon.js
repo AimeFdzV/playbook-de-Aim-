561af393b9a2c51d7339e15c957bf5f8d91296a2
Aimee Fernandez Vallina <Aimee@MacBook-de-Maria.local>
2022-04-12 23:39:18 -0500

    Subir archivos práctica1

---
+++ weekly_mission_1/example10/pokemon.js
@@ -0,0 +1,14 @@
+
+export default class Pokemon {
+    constructor(name) {
+      this.name = name;
+    }
+  
+    sayHello() {
+      console.log(`Hola, soy ${this.name}`);
+    }
+  
+    sayMessage(message) {
+      console.log(`${this.name} dice: ${message}`);
+    }
+  }
