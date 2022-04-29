e6ecdd4edf99d24baa2cd42836aeee62c4d7ea6b
AimeFdzV <99227728+AimeFdzV@users.noreply.github.com>
2022-04-28 23:50:03 -0500

    Subí ejemplos de operadores

---
+++ Operadores/ejemplo_5.js
@@ -0,0 +1,8 @@
+// Ejemplo 5: Uso de Map para convertir todos los nombres de una lista a mayúsculas
+const nombres = ['Asabeneh', 'Mathias', 'Elias', 'Brook']
+const nombresMayusculas = nombres.map((name) => name.toUpperCase())
+//const nombresMayusculas = nombres.map((name) => {return name.toUpperCase()})
+//nombres.forEach((name) => console.log(name.toUpperCase()))
+
+console.log(nombresMayusculas)
+//console.log(nombres)
