441ae6f14cf2df495283838bddcbaed42f1e423e
AimeFdzV <99227728+AimeFdzV@users.noreply.github.com>
2022-04-28 23:49:30 -0500

    Subí ejemplos de objetos

---
+++ weekly_mission_2/Objetos/ejemplo_1.js
@@ -0,0 +1,6 @@
+console.log ("Objetos")
+const miprimeravariable = {} 
+console.log ("Este es un obejto vacío")
+console.log (miprimeravariable)
+
+
---
+++ weekly_mission_2/Objetos/ejemplo_3.js
@@ -0,0 +1,19 @@
+// Ejemplo 3: Objeto con diferentes propiedades
+const myObject3 = {
+    name: "Aimé",
+    age: 25,
+    nicknames: [
+     "No",
+      "Tengo",
+      "Apodos"
+    ],
+    address: {
+      zip_code: "04200",
+      street: "Cerro de la Estrella 257",
+      city: "CDMX"
+    }
+  }
+  console.log("Ejemplo 3: Objeto con diferentes propiedades")
+myObject3       
+ 
+  
